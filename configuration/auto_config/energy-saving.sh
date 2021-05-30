0f4cd6839ddd29a2e957247cca2ca2313561baef
Tamino Bauknecht <dev@tb6.eu>
2021-05-30 12:11:13 +0200

    configuration guides added

---
+++ configuration/auto_config/energy-saving.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo systemctl enable --now tlp
+sudo systemctl enable --now tlp-sleep.service
+sudo systemctl enable NetworkManager-dispatcher.service
+sudo systemctl mask systemd-rfkill.service
+sudo systemctl mask systemd-rfkill.socket
