f429cd472487d228b1af719e8df278291f79e8aa
Tamino Bauknecht <dev@tb6.eu>
2021-12-19 15:04:26 +0100

    fixed battery_warning systemd timer

--- configuration/auto_config/install-battery-warning-service.sh
+++ configuration/auto_config/install-battery-warning-service.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cp -v /home/me/sync/config/configuration/scripts/battery_warning.sh /opt/bin/
+cp -v /home/me/sync/config/configuration/files/battery_warning.timer /usr/lib/systemd/user/
 cp -v /home/me/sync/config/configuration/files/battery_warning.service /usr/lib/systemd/user/
 
 # now service can be enabled using; do not execute as root! executed script is in user home:
