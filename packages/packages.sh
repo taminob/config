9d9333fc7ab77931550008375bf965b0514f7097
Tamino Bauknecht <dev@tb6.eu>
2022-01-06 22:44:40 +0100

    package list updated and comment support added

--- packages/packages.sh
+++ packages/packages.sh
@@ -8,7 +8,7 @@ fi
 PACKAGES_LISTS_PATH=$(dirname $0)
 
 for file_name in "$PACKAGES_LISTS_PATH"/*_packages; do
-	cat "$file_name"
+	sed -e 's/#.*//g' "$file_name"
 done
 
 cat "$PACKAGES_LISTS_PATH""/""$HOSTNAME""_packages_"
