9a43f7610158dadc50f78b2b618d9f19c760063f
Tamino Bauknecht <dev@tb6.eu>
2023-06-04 22:50:28 +0200

    sway: increase clipboard item limit to 128

--- sway/clipman.sh
+++ sway/clipman.sh
@@ -3,5 +3,5 @@
 app_id=$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true) | .app_id')
 if [[ $app_id != "org.keepassxc.KeePassXC" ]]; then
     # --no-persist so that we preserve rich text:
-    clipman store --no-persist
+    clipman store --no-persist --max-items 128
 fi
