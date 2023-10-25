5a39f6ffea70a9ea6ea2f791f456b439966e3d3f
Tamino Bauknecht <dev@tb6.eu>
2023-10-25 22:47:09 +0200

    configuration: add default applications

--- configuration/auto_config/default-apps.sh
+++ configuration/auto_config/default-apps.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
 
+set -e
+set -v
+
 # check current with e.g.:
 # xdg-mime query default text/plain
 
-# default file manager
-xdg-mime default ranger.desktop inode/directory
-xdg-mime default nvim.desktop text/plain
+FILE_MANAGER="ranger.desktop"
+TEXT_EDITOR="nvim.desktop"
+ARCHIVE_MANAGER="org.kde.ark.desktop"
+DOCUMENT_VIEWER="org.kde.okular.desktop"
+IMAGE_VIEWER="gwenview.desktop"
+XOURNALPP="com.github.xournalpp.xournalpp.desktop"
 
+# default file manager
+xdg-mime default "${FILE_MANAGER}" inode/directory
+
+xdg-mime default "${TEXT_EDITOR}" text/plain
+xdg-mime default "${TEXT_EDITOR}" inode/x-empty
+
+xdg-mime default "${ARCHIVE_MANAGER}" application/gzip
+xdg-mime default "${ARCHIVE_MANAGER}" application/zip
+
+xdg-mime default "${DOCUMENT_VIEWER}" application/pdf
+
+xdg-mime default "${IMAGE_VIEWER}" image/png
+
+xdg-mime default "${XOURNALPP}" application/x-xojpp
+xdg-mime default "${XOURNALPP}" application/x-xopt
+xdg-mime default "${XOURNALPP}" application/x-xopp
