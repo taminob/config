39728fa3cc1f62a37f83447070caee17839cffaf
Tamino Bauknecht <dev@tb6.eu>
2023-12-17 15:03:11 +0100

    auto_config: fix image viewer and add default video viewer

--- configuration/auto_config/default-apps.sh
+++ configuration/auto_config/default-apps.sh
@@ -10,8 +10,9 @@ FILE_MANAGER="ranger.desktop"
 TEXT_EDITOR="nvim.desktop"
 ARCHIVE_MANAGER="org.kde.ark.desktop"
 DOCUMENT_VIEWER="org.kde.okular.desktop"
-IMAGE_VIEWER="gwenview.desktop"
+IMAGE_VIEWER="org.kde.gwenview.desktop"
 XOURNALPP="com.github.xournalpp.xournalpp.desktop"
+VIDEO_VIEWER="vlc.desktop"
 
 # default file manager
 xdg-mime default "${FILE_MANAGER}" inode/directory
@@ -25,6 +26,10 @@ xdg-mime default "${ARCHIVE_MANAGER}" application/zip
 xdg-mime default "${DOCUMENT_VIEWER}" application/pdf
 
 xdg-mime default "${IMAGE_VIEWER}" image/png
+
+xdg-mime default "${VIDEO_VIEWER}" video/x-msvideo
+xdg-mime default "${VIDEO_VIEWER}" video/mp4
+xdg-mime default "${VIDEO_VIEWER}" video/x-matroska
 
 xdg-mime default "${XOURNALPP}" application/x-xojpp
 xdg-mime default "${XOURNALPP}" application/x-xopt
